a7f1e2d0a25c4b586615973d4baca8e2dcbccebf
DarrenN <info@v25media.com>
2018-09-03 13:06:12 -0400

    Update README, commentblock and some tests

--- README.md
+++ README.md
@@ -3,6 +3,35 @@ yaml2json
 
 Racket flavored version of `yaml2json`
 
+Super simple YAML to JSON converter, meant for use as a CLI command. Will take a filename to open and parse or read from `stdin`.
+
+### To use:
+
+``` shell
+$ yaml2json alpha.yaml // will emit a JSON string in the terminal
+$ yaml2json -o alpha.json alpha.yaml // will save JSON to alpha.json
+$ cat alpha.yaml | yaml2json // will emit a JSON string in the terminal
+$ cat .travis.yml | yaml2json | json_pp // will emit nicely formatted JSON
+```
+
+### To install:
+
+``` shell
+$ ./install.sh
+```
+
+The shell script will build a Racket binary and copy to `/usr/local/bin/yaml2json`.
+
+### Prerequisites:
+
+You will need a [Racket](https://racket-lang.org/).
+
+### License
+
+[LGPL](https://github.com/DarrenN/yaml2json/blob/master/LICENSE.txt)
+
+<a href="https://racket-lang.org/"><img src="https://racket-lang.org/img/racket-logo.svg" width="100" height="100" alt="Racket logo" title="Racket" /></a>
+
 ---
 
 Copyright 2018 Darren Newton
--- main.rkt
+++ main.rkt
@@ -1,66 +1,57 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
-
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
+;; yaml2json - copyright 2018 DarrenN
 ;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
+;; Super simple YAML to JSON converter, meant for use as a CLI command.
+;; Will take a filename to open and pars or stdin.
 ;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+;; $ yaml2json alpha.yaml // will emit a JSON string in the terminal
+;; $ yaml2json -o alpha.json alpha.yaml // will save JSON to alpha.json
+;; $ cat alpha.yaml | yaml2json // will emit a JSON string in the terminal
+;; $ cat .travis.yml | yaml2json | json_pp // will emit nicely formatted JSON
 
-;; Code here
+(require json
+         yaml
+         racket/bool
+         racket/contract
+         racket/contract/region
+         racket/port)
 
-(module+ test
-  ;; Tests to be run with raco test
-  )
+;; Recursively convert hash -> hasheq which is a valid jsexpr?
+(define/contract (hash->hasheq hsh)
+  (-> any/c any/c)
+  (cond [(list? hsh) (map hash->hasheq hsh)]
+        [(not (or (hash? hsh) (list? hsh))) hsh]
+        [else
+         (let ([keys (hash-keys hsh)])
+           (foldl (λ (key result)
+                    (hash-set result (string->symbol key)
+                              (hash->hasheq (hash-ref hsh key))))
+                  (make-immutable-hasheq) keys))]))
+
+;; Convert YAML to JSON and either send to stdout or to a file
+(define/contract (run in-port filename)
+  (-> port? (or/c string? boolean?) void?)
+  (let* ([contents (port->string in-port)]
+         [yaml (string->yaml contents)]
+         [json (jsexpr->string (hash->hasheq yaml))])
+    (if (false? filename)
+        (displayln json (current-output-port))
+        (call-with-output-file*
+          filename
+          (λ (out) (displayln json out))
+          #:mode 'text #:exists 'replace))))
+
+
+;//////////////////////////////////////////////////////////////////////////////
+; PUBLIC
 
 (module+ main
-  (require json
-           yaml
-           racket/bool
-           racket/cmdline
-           racket/port)
-
-  ;; Recursively convert hash -> hasheq which is a valid jsexpr?
-  (define (hash->hasheq hsh)
-    (cond [(list? hsh) (map hash->hasheq hsh)]
-          [(not (or (hash? hsh) (list? hsh))) hsh]
-          [else
-           (let ([keys (hash-keys hsh)])
-             (foldl (λ (key result)
-                      (hash-set result (string->symbol key)
-                                (hash->hasheq (hash-ref hsh key))))
-                    (make-immutable-hasheq) keys))]))
-
-  (define (run in-port filename)
-    (let* ([contents (port->string in-port)]
-           [yaml (string->yaml contents)]
-           [json (jsexpr->string (hash->hasheq yaml))])
-      (if (false? filename)
-          (displayln json (current-output-port))
-          (call-with-output-file*
-            filename
-            (λ (out) (displayln json out))
-            #:mode 'text #:exists 'replace))))
+  (require racket/cmdline)
 
   (define output-filename (make-parameter #f))
 
+  ;; Parse command line args
   (define convert-yaml
     (command-line
      #:program "yaml2json"
@@ -75,3 +66,37 @@
           (current-input-port)
           (open-input-file (car filename)))
       (output-filename)))))
+
+;//////////////////////////////////////////////////////////////////////////////
+; TESTS
+
+(module+ test
+  (require rackunit
+           racket/contract)
+
+  (check-not-exn (λ () (contract-exercise hash->hasheq 100)))
+
+  (for ([i (in-range 50)])
+    (let ([x (contract-random-generate any/c)])
+      (check-eq? x (hash->hasheq x) "output not equal to input?")))
+
+  (define hsh (hash->hasheq (hash "alpha" 12 "β" (hash "gamma" 13))))
+  (check-pred hash-eq? hsh "isn't a hash-eq?")
+  (check-pred immutable? hsh "isn't immutable?")
+
+  (check-not-exn (λ () (contract-exercise run 100)))
+
+  (define yaml-port (open-input-string "
+alpha: 12
+beta:
+ - gamma: 2
+ - delta: phi
+"))
+
+  (define out (open-output-string))
+  (parameterize ([current-output-port out])
+    (run yaml-port #f)
+    (check-equal?
+     "{\"alpha\":12,\"beta\":[{\"gamma\":2},{\"delta\":\"phi\"}]}\n"
+     (get-output-string out)
+     "Parses yaml from input-port? into json string in output-port?")))
